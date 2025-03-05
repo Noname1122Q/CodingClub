1baf6af13fe9b5a281cb696c8c2bd76480b25236
uttam <uttamjangir14@gmail.com>
2025-03-05 18:13:48 +0530

    events ui updates

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,12 +1,23 @@
 import Calendar from "@/components/events/DynamicCalendar";
 import { EventsTab } from "@/components/events/EventsTab";
-
 import React from "react";
 
 const EventsPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full space-y-4 md:space-y-6">
-      <Calendar />
+      <div className="flex flex-col lg:flex-row p-4 justify-around space-x-4">
+        <div className="flex flex-col text-center items-center justify-center max-w-lg">
+          <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-center text-[#CF2027]">
+            Our Events
+          </h2>
+          <p className="text-sm md:text-lg lg:text-xl text-wrap">
+            Never miss an exciting event! Our club hosts coding sessions, tech
+            talks, and hands-on workshops. Check the calendar to see what's
+            coming up!
+          </p>
+        </div>
+        <Calendar />
+      </div>
       <EventsTab />
     </div>
   );
--- components/events/DynamicCalendar.tsx
+++ components/events/DynamicCalendar.tsx
@@ -86,10 +86,8 @@ const Calendar = () => {
                 <HoverCardContent className="w-80">
                   <div className="flex justify-between space-x-4">
                     <div className="space-y-1">
-                      <h4 className="text-sm font-semibold">@nextjs</h4>
-                      <p className="text-sm">
-                        The React Framework created and maintained by @vercel.
-                      </p>
+                      <h4 className="text-sm font-semibold">{event.name}</h4>
+                      <p className="text-sm">{event.date}</p>
                     </div>
                   </div>
                 </HoverCardContent>
