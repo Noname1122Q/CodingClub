27346abf5f1f09ba83398608d51c8a51e5452627
uttam <uttamjangir14@gmail.com>
2025-03-08 13:08:41 +0530

    github link added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import MenuBar from "./MenuBar";
 import { useRouter } from "next/navigation";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   const router = useRouter();
@@ -12,9 +13,14 @@ const Navbar = () => {
     <nav className="bg-transparent shadow-md w-full left-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-around md:justify-between h-16 items-center">
-          <Link href="/" className="flex items-center">
-            <Image src="/logo.jpeg" alt="Logo" width={50} height={50} />
-          </Link>
+          <div className="flex space-x-2 items-center justify-center">
+            <Link href="/" className="flex items-center">
+              <Image src="/logo.jpeg" alt="Logo" width={50} height={50} />
+            </Link>
+            <Link href={"https://github.com/Noname1122Q/CodingClub"}>
+              <FaGithub className="size-6 ml-2" />
+            </Link>
+          </div>
           <div className="hidden md:flex space-x-6">
             <Link
               href="/"
